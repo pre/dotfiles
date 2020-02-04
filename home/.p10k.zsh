62c73a1bf9634e57104b068828f7bd9c090029e9
Petrus Repo <petrus.repo@iki.fi>
2020-02-04 14:43:07 +0200

    (zsh) Add kube-fzf to show on command

--- home/.p10k.zsh
+++ home/.p10k.zsh
@@ -814,7 +814,7 @@
   #############[ kubecontext: current kubernetes context (https://kubernetes.io/) ]#############
   # Show kubecontext only when the the command you are typing invokes one of these tools.
   # Tip: Remove the next line to always show kubecontext.
-  typeset -g POWERLEVEL9K_KUBECONTEXT_SHOW_ON_COMMAND='_rex_safe|rex|k|kubectl|kns|kctx|helm|kubens|kubectx|oc'
+  typeset -g POWERLEVEL9K_KUBECONTEXT_SHOW_ON_COMMAND='_rex_safe|rex|k|kubectl|kns|kctx|execpod|tailpod|findpod|describepod|pfpod|helm|kubens|kubectx|oc'
 
   # Kubernetes context classes for the purpose of using different colors, icons and expansions with
   # different contexts.
